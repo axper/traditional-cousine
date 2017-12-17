16b54fec0c44a960562e0b2d166a21a2490a0af4
Babken Vardanyan <babken@lazydevelo.com>
2017-12-17 22:17:25 +0400

    Fixed settings for static and media

--- traditional_cousine/urls.py
+++ traditional_cousine/urls.py
@@ -21,6 +21,12 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls', namespace='core')),
-] \
-              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
